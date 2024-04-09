a971b9d3d809eeae0d196851f01f4b69588b5d9c
Daniel Freitas <daniel.275622@alunosatc.edu.br>
2024-04-09 20:54:53 -0300

    Feat:Criando componentes

---
+++ frontend/src/components/Cards/Cards.tsx
@@ -0,0 +1,9 @@
+const Cards = () => {
+    return(
+        <div>
+            <h1>title</h1> 
+            <p>text</p>
+        </div> 
+    )
+}
+export default Cards;
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
+import Cards from "../components/Cards/Cards";
+const Home = () => {
+    return (
+        <>
+        <Header/>
+        <Cards/>
+        <Footer/>
+        </>
+    )
+}
+export default Home;
